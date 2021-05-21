ac1062f0b1664f716be7cc40a4cf122d93a17219
Alejandro Torres <atorresalcala.me@gmail.com>
2021-05-21 12:40:09 +0200

    build(app): initial structure uploaded

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Assets/index.css';
-import Calculator from './Pages/Calculator';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Calculator />
+		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
